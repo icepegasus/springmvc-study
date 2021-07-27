e6781ef0ccf4ad93e6d72940b9b5db021f08fe37
DESKTOP-HO84V0D\hoya <icepega@gmail.com>
2021-07-28 00:46:28 +0900

    json 출력

--- src/main/java/com/newlecture/web/controller/api/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/api/NoticeController.java
@@ -1,16 +1,29 @@
 package com.newlecture.web.controller.api;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @RestController("apiNoticeController")
 @RequestMapping("/api/notice")
 public class NoticeController {
 	
+	
+	@Autowired
+	private NoticeService noticeService;
+	
 	@RequestMapping("/list")
-	public String list() {
+	public List<Notice> list() throws ClassNotFoundException, SQLException {
 		
-		return "공지사항 list";
+		List<Notice> list =  noticeService.getList(1, "TITLE", "");
+		
+		return list;
 		
 	}
 
