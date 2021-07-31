9c4c02bd047e9e82e0ad24518373d9b157e74bee
DESKTOP-HO84V0D\hoya <icepega@gmail.com>
2021-08-01 02:57:28 +0900

    파일업로드 multipartResolver , multipart/form-data

--- src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -3,6 +3,7 @@ package com.newlecture.web.controller.admin.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice")
@@ -17,7 +18,11 @@ public class NoticeController {
 	
 	@RequestMapping("/reg")
 	@ResponseBody
-	public String reg(String title, String content, String category, String[] foods, String food) {
+	public String reg(String title, String content, String category, String[] foods, String food, MultipartFile file) {
+		
+		long size = file.getSize();
+		String fileName =  file.getOriginalFilename();
+		System.out.printf("fileName:%s, fileSize:%d\n",fileName, size);
 		
 		for(String f : foods) {
 			System.out.println("checkbox food : "+f);
