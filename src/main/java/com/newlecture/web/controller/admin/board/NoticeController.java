447cecc88b3bcc7fb26fc9efe8706fcd5362d1c0
DESKTOP-HO84V0D\hoya <icepega@gmail.com>
2021-08-01 03:48:43 +0900

    물리경로얻기+파일저장

--- src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -1,5 +1,12 @@
 package com.newlecture.web.controller.admin.board;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +15,9 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice")
 public class NoticeController {
+	
+	@Autowired
+	private ServletContext ctx;
 	
 	
 	@RequestMapping("/list")
@@ -18,11 +28,31 @@ public class NoticeController {
 	
 	@RequestMapping("/reg")
 	@ResponseBody
-	public String reg(String title, String content, String category, String[] foods, String food, MultipartFile file) {
+	public String reg(String title, String content, String category, String[] foods, String food, MultipartFile file, HttpServletRequest request) throws IllegalStateException, IOException{
 		
 		long size = file.getSize();
 		String fileName =  file.getOriginalFilename();
 		System.out.printf("fileName:%s, fileSize:%d\n",fileName, size);
+		
+		//ServletContext ctx = request.getServletContext();
+		 
+		String webPath = "/static/upload";
+		String realPath = ctx.getRealPath(webPath);
+		System.out.printf("realPath : %s\n", realPath);
+		
+		//업로드 경로가 없을경우 디렉토리 생성
+		File savePath = new File(realPath);
+		if(!savePath.exists()) {
+			savePath.mkdirs();
+		}
+		
+		realPath += File.separator + fileName;
+		
+		System.out.printf("last realPath : %s\n", realPath);
+		
+		File saveFile = new File(realPath);
+		file.transferTo(saveFile);
+		
 		
 		for(String f : foods) {
 			System.out.println("checkbox food : "+f);
